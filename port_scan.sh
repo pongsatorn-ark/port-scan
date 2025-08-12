a1d9b1e2190cdf24fd2365dc172881a60b681d6c
pongsatorn-ark <pongsatorn-ark>
2025-08-13 03:51:27 +0700

    script

---
+++ port_scan.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+target="$1"
+output_base="nmap-initiate"
+current_path=$(pwd)
+
+echo $current_path
+
+echo "==== TCP Port Scan (Full) ===="
+/usr/bin/nmap -sS -p- -T4 -Pn "$target" -oA "${current_path}/${output_base}_tcp_init" -v
+
+echo "==== UDP Port Scan (Top 100) ===="
+/usr/bin/nmap -sU --top-ports 100 -T4 -Pn "$target" -oA "${current_path}/${output_base}_udp_init" -v
+
+echo "==== Filter Ports ===="
+# Filter TCP ports
+port_range_tcp=$(grep "Ports:" "${current_path}/${output_base}_tcp_init.gnmap" | grep "open" | \
+  sed -n 's/.*Ports: \(.*\)/\1/p' | tr ',' '\n' | grep '/open/tcp' | \
+  cut -d '/' -f1 | sed 's/^ *//' | sort -n | uniq | paste -sd,)
+
+# Filter UDP ports
+port_range_udp=$(grep "Ports:" "${current_path}/${output_base}_udp_init.gnmap" | grep "open" | \
+  sed -n 's/.*Ports: \(.*\)/\1/p' | tr ',' '\n' | grep '/open/udp' | \
+  cut -d '/' -f1 | sed 's/^ *//' | sort -n | uniq | paste -sd,)
+
+echo "TCP Ports: $port_range_tcp"
+echo "UDP Ports: $port_range_udp"
+
+echo "==== Scan Port Detail ===="
+
+# TCP Detail Scan
+if [[ -n "$port_range_tcp" ]]; then
+  echo "→ TCP detail scan"
+  /usr/bin/nmap -A -sS -T4 -Pn "$target" -oA "${current_path}/${output_base}_tcp" --stats-every=1m -p "$port_range_tcp"
+fi
+
+# UDP Detail Scan
+if [[ -n "$port_range_udp" ]]; then
+  echo "→ UDP detail scan"
+  /usr/bin/nmap -A -sU -T4 -Pn "$target" -oA "${current_path}/${output_base}_udp" --stats-every=1m -p "$port_range_udp"
+fi
+
+rm "${current_path}/${output_base}_udp_init.*"
+rm "${current_path}/${output_base}_tcp_init.*"
+
+echo "==== Done ===="
